a4cdcba1c6a14892a8097772443129138d3a6a45
selloriwoo <sellossier@gmail.com>
2026-06-07 21:13:08 +0900

    add 15.2

--- src/smartpointer_f.rs
+++ src/smartpointer_f.rs
@@ -1,5 +1,26 @@
 use crate::smartpointer_f::List::{Cons, Nil} ;
 // use List::{Cons, Nil};
+use std::ops::Deref;
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+fn hello(name: &str) {
+    println!("Hello, {name}!");
+}
+
+impl<T> Deref for MyBox<T>{
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
 enum List {
     Cons(i32, Box<List>),
     Nil,
@@ -11,4 +32,14 @@ pub fn smartpointer_file() {
     println!("box ={b}");
 
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    let x = 5;
+    let y = Box::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
+
 }
