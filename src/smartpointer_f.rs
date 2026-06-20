19a63f2bc27c2d07fa0795eaca0c2280a7e3b989
selloriwoo <sellossier@gmail.com>
2026-06-20 15:35:20 +0900

    add 15.3, 15.4

--- src/smartpointer_f.rs
+++ src/smartpointer_f.rs
@@ -1,7 +1,8 @@
-use crate::smartpointer_f::List::{Cons, Nil} ;
+use crate::smartpointer_f::List::{Cons, Nil};
 // use List::{Cons, Nil};
 use std::ops::Deref;
 struct MyBox<T>(T);
+
 
 impl<T> MyBox<T> {
     fn new(x: T) -> MyBox<T> {
@@ -26,6 +27,22 @@ enum List {
     Nil,
 }
 
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
+enum List2 {
+    Cons(i32, Rc<List2>),
+    Nil,
+}
+
+use std::rc::Rc;
 
 pub fn smartpointer_file() {
     let b = Box::new(5);
@@ -39,7 +56,38 @@ pub fn smartpointer_file() {
     assert_eq!(5, x);
     assert_eq!(5, *y);
 
+    //drop
     let m = MyBox::new(String::from("Rust"));
     hello(&m);
 
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    println!("CustomSmartPointers created");
+
+
+    let c = CustomSmartPointer {
+        data: String::from("some data"),
+    };
+    println!("CustomSmartPointer created");
+    drop(c);
+    println!("CustomSmartPointer dropped before the end of main");
+
+    //Rc
+    let a = Rc::new(List2::Cons(5, Rc::new(List2::Cons(10, Rc::new(List2::Nil)))));
+    let b = List2::Cons(3, Rc::clone(&a));
+    let c = List2::Cons(4, Rc::clone(&a));
+
+    let a = Rc::new(List2::Cons(5, Rc::new(List2::Cons(10, Rc::new(List2::Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = List2::Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = List2::Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
